00493ac74994b3ef071d28adfbf35e98fba3d412
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 03:10:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require 'pry'
 def load_library(path)
   emoticons = YAML.load_file(path)
 
-  library = {get_meaning => {}, get_emoticon => {}}
+  library = {"get_meaning" => {}, "get_emoticon" =>{}}
 
 end
 
