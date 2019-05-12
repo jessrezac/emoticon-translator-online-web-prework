50ef5f89082009b788f119ddc5196ba958638a18
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 03:04:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,11 +8,7 @@ def load_library(path)
   # this needs to return { get_emoticon => japanese, get_meaning => english }
   translation_key = Hash.new
 
-  emoticons.each do |emoticon_name, interpretations|
-    translation_key[:get_meaning[emoticon_name]] = interpretations[0]
-    translation_key[:get_emoticon[emoticon_name]] = interpretaions[1]
-    binding.pry
-  end
+
 
   translation_key
 end
