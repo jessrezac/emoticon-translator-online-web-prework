5b91ddbaa6b88db298508bbd7b8e265980858f2b
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 02:56:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,8 @@ def load_library(path)
   emoticons = YAML.load_file(path)
 
   # this needs to return { get_emoticon => japanese, get_meaning => english }
+  get_emoticon = Hash.new
+  get_meaning = Hash.new
 
   emoticons.each do |emoticon_name, interpretations|
     binding.pry
