9f1ce6f98293b89f524dc475c2aaf1d8ce04807f
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 03:24:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(path, emoticon)
     if eng_emoticon == emoticon
       return jpn_emoticon
     else
-      return "Sorry, that emoticon was not found"
+      puts "Sorry, that emoticon was not found"
     end
   end
 end
